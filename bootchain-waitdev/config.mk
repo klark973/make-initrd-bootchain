ddf46041823f9e2ab87bde606c41924c6caec857
Leonid Krivoshein <klark@altlinux.org>
2021-09-23 15:11:16 +0300

    cdrom_id now added by tools/create-initrd

--- bootchain-waitdev/config.mk
+++ bootchain-waitdev/config.mk
@@ -3,5 +3,4 @@ $(call feature-requires,bootchain-core)
 BOOTCHAIN_WAITDEV_DATADIR = $(FEATURESDIR)/bootchain-waitdev/data
 
 BOOTCHAIN_WAITDEV_FILES = \
-	/lib/udev/rules.d/60-cdrom_id.rules \
-	/lib/udev/cdrom_id \
+	/lib/udev/rules.d/60-cdrom_id.rules
