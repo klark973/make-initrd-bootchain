aff3673db9b71d324cb2abdea646635d150d53df
Leonid Krivoshein <klark@altlinux.org>
2021-10-07 14:23:12 +0300

    bootchain-waitdev: use add-udev-rules instead put files

--- bootchain-waitdev/config.mk
+++ bootchain-waitdev/config.mk
@@ -1,6 +1,6 @@
-$(call feature-requires,bootchain-core)
+$(call feature-requires,bootchain-core add-udev-rules)
 
 BOOTCHAIN_WAITDEV_DATADIR = $(FEATURESDIR)/bootchain-waitdev/data
 
-BOOTCHAIN_WAITDEV_FILES = \
-	/lib/udev/rules.d/60-cdrom_id.rules
+BOOTCHAIN_WAITDEV_RULES = \
+	*-cdrom_id.rules
