5dbb65ea1384f44546989ea228c39b96843548c6
Leonid Krivoshein <klark@altlinux.org>
2023-07-08 23:33:10 +0300

    bootchain-core: pack optional feature config file

--- bootchain-core/config.mk
+++ bootchain-core/config.mk
@@ -4,3 +4,5 @@ $(call feature-disables,pipeline)
 BOOTCHAIN_CORE_DATADIR = $(FEATURESDIR)/bootchain-core/data
 
 BOOTCHAIN_CORE_MODULES = isofs squashfs overlay
+
+BOOTCHAIN_CORE_FILES = $(shell [ ! -f /etc/sysconfig/bootchain ] || echo -n "/etc/sysconfig/bootchain")
