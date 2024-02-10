61fd5aecdc74e5eb303f12b84dc167b2881dbd66
Leonid Krivoshein <klark@altlinux.org>
2024-02-11 02:16:42 +0300

    core+interactive: daemon stop has been reworked

--- bootchain-interactive/config.mk
+++ bootchain-interactive/config.mk
@@ -2,6 +2,6 @@ $(call feature-requires,bootchain-core)
 
 BOOTCHAIN_INTERACTIVE_DATADIR = $(FEATURESDIR)/bootchain-interactive/data
 
-BOOTCHAIN_INTERACTIVE_PROGS = chvt dialog less openvt pv
+BOOTCHAIN_INTERACTIVE_PROGS = chvt dialog less openvt deallocvt pv
 
 BOOTCHAIN_INTERACTIVE_FILES = $(shell [ ! -f /etc/dialogrc.error ] || echo -n "/etc/dialogrc.error")
